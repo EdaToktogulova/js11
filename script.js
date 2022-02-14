fba3ef3d136e592edb4afba051a5e5895f24ea50
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-02-14 14:42:43 +0600

    added new file

---
+++ script.js
@@ -0,0 +1,18 @@
+// document.querySelector('a').addEventListener('click', function(event) {
+//    event.preventDefault();
+//    this.textContent = "Yandax";
+// });
+// document.querySelector('a').addEventListener('contextmenu', function(event) {
+//    event.preventDefault();
+//    this.style.background ="red";
+// });
+// document.querySelector('a').addEventListener('dblclick', function(event) {
+//    event.preventDefault();
+//    this.remove();
+// });
+
+
+
+document.querySelector('div').addEventListener('click', function(event) {
+   event.preventDefault();
+})
